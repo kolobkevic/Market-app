04b16e018bf92863e9c6741921070bcd9e9a9f18
kolobkevic <kolobkevic@gmail.com>
2022-11-27 21:52:12 +0300

    Добавил работу страницу с заказами

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -26,6 +26,10 @@
                 templateUrl: 'cart/cart.html',
                 controller: 'cartController'
             })
+            .when('/orders', {
+                templateUrl: 'orders/orders.html',
+                controller: 'ordersController'
+            })
             .otherwise({
                 redirectTo: '/'
             });
